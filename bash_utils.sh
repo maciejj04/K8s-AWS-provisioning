6acbd030649a81e4b94b3a190c41f5e222541050
Maciej Józefczyk <maciejj04@gmail.com>
2018-08-18 22:39:19 +0200

    Fixed arg validation

--- bash_utils.sh
+++ bash_utils.sh
@@ -1,15 +1,11 @@
 
 function checkArgs() {
 	echo "Checking args..."
-	argsCount=$#
 
-	for x in $(seq 1 1 $#); do
-		echo $x
-		if [ "$x" = "" ]; then
-			echo "dupa"
-			exit 1
-		fi
-	done
+	count=$1
+	if [[ $count -ne $(($#-1)) ]]; then
+		echo "ERROR: Wrong nr of arguments for $0"
+	fi
 	
 }
 
@@ -17,7 +13,7 @@ function randomAlphanumericClusterResourceIdentifier() {
 	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1
 }
 
-checkArgs dupa a a
+checkArgs 4 dupa a a 
 
 #res=$(randomAlphanumericClusterResourceIdentifier)
 #echo $res
